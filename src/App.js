b4b15aaea03d59cea8ac9dcc14a8880e68c97938
Dorota Rankowska <dorota.rankowska@gmail.com>
2020-10-15 22:39:41 +0200

    with delete and add button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React, { useEffect, useState } from 'react';
-import { getTodos } from './Requests'
+import { addTodo, deleteTodo, getTodos } from './Requests'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
 function App() {
   const [todos, updateTodos] = useState([]);
-  
+  const [newInputValue, setNewInputValue] = useState('');
+  const [newInputDesc, setNewInputDesc] = useState('');
+
   useEffect(() => {
+    getAndRenderTodos()
+  }, [])
+  const getAndRenderTodos = () => {
     getTodos().then(response => {
       const { data } = response;
       updateTodos(data);
     });
-  }, [])
+  }
+  const onDelete = (id) => {
+    deleteTodo(id)
+    getAndRenderTodos()
+  }
+  const addNewTask = (event) => {
+    event.preventDefault();
+    addTodo({
+      title: newInputValue,
+      description: newInputDesc
+    }).then(() => {
+      getAndRenderTodos()
+    })
+  }
   return (
     <main>
-      <ul>
-        {todos.map(todo => {
-          return <li>{todo.title}</li>
-        })
-        }
+      <div className="container">
+        <h1> Create a task</h1>
+        <form>
+          <label forhtml ="taskTitle">Provide a task name </label>
+          <input onChange ={(event) => { setNewInputValue(event.target.value)}}type="text" id="taskTitle" />
+          <label forhtml ="taskDesc">Provide a task description </label>
+          <input onChange  ={(event) => { setNewInputDesc(event.target.value)}}type="text" id="taskDesc" />
+          <button onClick={addNewTask} type="submit">ADD</button>
+        </form>
 
-      </ul>
+        <h1> List of tasks</h1>
+        {todos.map(todo => {
+          return (
+          <div className="todo" key={todo.id}>
+            <h2>{todo.title}</h2>
+            <p>{todo.description}</p>
+            <button onClick={() => {onDelete(todo.id) }} className="btn btn-danger">DELETE</button>
+          </div>
+          ) 
+        }
+        )}
+      </div>
     </main>
   );
 }
--- src/Requests/index.js
+++ src/Requests/index.js
@@ -4,3 +4,11 @@ import axios from "axios"
 export const getTodos = () => {
     return axios.get('http://51.75.120.145:3000/todo');
 }
+
+export const deleteTodo = id => {
+    return axios.delete(`http://51.75.120.145:3000/todo/${id}`)
+}
+
+export const addTodo = task => {
+    return axios.post('http://51.75.120.145:3000/todo', task)
+}
